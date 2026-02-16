6e9fc06a36a2f3e7641d09ae7a7eda5bb69eb9b8
Valnaztra <125087137+Cadejo77@users.noreply.github.com>
2026-02-16 17:43:18 -0500

    Delete engine directory

--- engine/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Synapse Engine - Python implementation of the compositional prompt generator
-"""
-
-from .generator import PromptGenerator
-from .loader import load_all_config
-
-__all__ = ['PromptGenerator', 'load_all_config']
